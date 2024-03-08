1de27a6aafb38203a11e9cdc8e2b5b0480a3f25c
Losalini Rokocakau <c.rokocakau@gmail.com>
2024-03-09 01:32:55 +1200

    Add navbar component with its assets

--- figure-rate/src/App.tsx
+++ figure-rate/src/App.tsx
@@ -1,7 +1,23 @@
 import "./App.css";
+import Navbar from "./components/Navbar";
+import figureRateLogo from "./assets/figure-rate-logo-1.png";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
 function App() {
-  return <></>;
+  let menuItems = {
+    items: ["Term Deposits", "About", "Contact"],
+    menuFilePath: [
+      "./components/TermDeposits.tsx",
+      "./components/About.tsx",
+      "./components/Contact.tsx",
+    ],
+  };
+
+  return (
+    <>
+      <Navbar imgSrcPath={figureRateLogo} navMenuItems={menuItems} />
+    </>
+  );
 }
 
 export default App;
