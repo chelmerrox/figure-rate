23e70207651cd013943a45192c1703c041ae249f
Losalini Rokocakau <c.rokocakau@gmail.com>
2024-03-14 23:09:01 +1200

    Add TermDepositsBody component

--- figure-rate/src/components/TermDepositsBody.tsx
+++ figure-rate/src/components/TermDepositsBody.tsx
@@ -1,9 +1,20 @@
 import { useState } from "react";
 import "../App.css";
 
+interface FinancialInstitutions {
+  financialInstitution: [
+    {
+      financialInstitutionID: number;
+      financialInstitutionName: string;
+      interestRates: string[];
+    }
+  ];
+}
+
 interface TermDepositsBodyProps {
   tableHeadings: string[];
-  tableBody: string[][];
+  tableBody: FinancialInstitutions;
+  //tableBody: string[][];
 }
 
 function TermDepositsBody({ tableHeadings, tableBody }: TermDepositsBodyProps) {
@@ -15,28 +26,35 @@ function TermDepositsBody({ tableHeadings, tableBody }: TermDepositsBodyProps) {
         </h1>
 
         <div className="table-responsive mt-5">
-          <table className="table table-striped table-hover">
+          <table className="table table-hover">
             <thead className="table-header table-light">
               <tr>
                 {tableHeadings.map((heading, i) => (
-                  <th key={i} scope="col" className="text-center">
-                    {heading}
+                  <th
+                    key={i}
+                    scope="col"
+                    className={
+                      i === 0 || i === 1 ? "text-start" : "text-center"
+                    }
+                  >
+                    {i === 0 || i === 1 || i === 7 || i === 8
+                      ? heading
+                      : heading + " months"}
                   </th>
                 ))}
               </tr>
             </thead>
-            <tbody className="table-group-divider">
-              {tableBody.map((row, i) => (
-                <tr key={i + 1}>
-                  {row.map((item, j) => (
-                    <td
-                      className={
-                        j === 0 ? "text-center fw-bolder" : "text-center"
-                      }
-                      scope={j === 0 ? "row" : ""}
-                    >
-                      {j === 0 ? item + " months" : item}
-                    </td>
+            <tbody>
+              {tableBody.financialInstitution.map((institution, j) => (
+                <tr key={j} className={"row-" + (j + 1)}>
+                  <th className="text-start">
+                    {institution.financialInstitutionID}
+                  </th>
+                  <td className="text-start">
+                    {institution.financialInstitutionName}
+                  </td>
+                  {institution.interestRates.map((rate) => (
+                    <td className="text-center">{rate}</td>
                   ))}
                 </tr>
               ))}
@@ -49,3 +67,34 @@ function TermDepositsBody({ tableHeadings, tableBody }: TermDepositsBodyProps) {
 }
 
 export default TermDepositsBody;
+
+/*
+{tableBody.map((row, i) => (
+  <tr key={i + 1}>
+    {row.map((item, j) => (
+      <td
+        className={
+          j === 0
+            ? "text-center fw-bolder bg-light"
+            : "text-center"
+        }
+        scope={j === 0 ? "row" : ""}
+      >
+        {j === 0 ? item + " months" : item}
+      </td>
+    ))}
+  </tr>
+))}
+*/
+
+/* 
+    bankOfBaroda: string[];
+    bankOfSouthPacific: string[];
+    creditCorporationFiji: string[];
+    fijiDevelopmentBank: string[];
+    fijianHoldingdUnitTrust: string[];
+    homeFinanceCompanyBank: string[];
+    kontikiFinance: string[];
+    merchantFinance: string[];
+    westpac: string[];
+*/
