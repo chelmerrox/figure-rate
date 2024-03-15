2a7edecde29cd3f4d44b127ba9e16b6eaf62a8d5
Losalini Rokocakau <c.rokocakau@gmail.com>
2024-03-15 16:44:31 +1200

    Add HeadingOne component

---
+++ figure-rate/src/components/HeadingOne.tsx
@@ -0,0 +1,11 @@
+function HeadingOne() {
+  return (
+    <>
+      <h1 className="league-spartan-font text-center my-5">
+        Compare interest rates with <span className="font-purple">ease.</span>
+      </h1>
+    </>
+  );
+}
+
+export default HeadingOne;
